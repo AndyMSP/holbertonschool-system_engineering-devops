13a2f9586a60ade44ff19e65a908813119eb2954
Andy <3775@holbertonschool.com>
2022-08-23 17:35:53 -0400

    Add tasks 0 and 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,59 @@
+# Install and configure nginx on server
+
+# Ensure nginx is installed
+package{ 'nginx':
+    ensure   => latest,
+    provider => apt
+}
+
+# Ensure default config file is gone
+file{ '/etc/nginx/sites-enabled/default':
+    ensure => absent
+}
+
+# Ensure tacobell directory is present in www
+file{ '/var/www/tacobell':
+    ensure => directory
+}
+
+# Ensure tacobell index file is present
+file{ '/var/www/tacobell/index.html':
+    ensure  => 'present',
+    content => "Hello World\n"
+}
+
+# Ensure custom 404 page is present
+file{ '/var/www/tacobell/custom_404.html':
+    ensure  => 'present',
+    content => "Ceci n'est pas une page applied with puppet\n"
+}
+
+# Server config file
+file{ '/etc/nginx/conf.d/tacobell.conf':
+    ensure  => 'present',
+    content =>
+    "server{
+	listen 80;
+	root /var/www/tacobell;
+	index index.html;
+
+    add_header X-Served-By ${hostname};
+
+	location /redirect_me {
+		return 301 http://churger.tech/;
+	}
+
+	error_page 404 /custom_404.html;
+	location = /custom_404.html {
+		root /var/www/tacobell;
+		internal;
+	}
+
+}"
+}
+
+
+# Restart nginx server
+exec{ 'restart_server':
+    command => '/usr/sbin/service nginx restart'
+}
