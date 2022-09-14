77a13b15ff221fb785566e0728adb9b007b64b81
Andy <3775@holbertonschool.com>
2022-09-14 16:40:31 -0400

    Add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# Script interacts with API to retrieve and present data
+
+from sys import argv
+from requests import get
+import json
+
+emp_id = argv[1]
+
+user = get('https://jsonplaceholder.typicode.com/users/{}'.format(emp_id))\
+    .json()
+EMPLOYEE_NAME = user.get('name')
+
+todos = get(
+    'https://jsonplaceholder.typicode.com/todos?userId={}'
+    .format(emp_id)).json()
+
+TOTAL_NUMBER_OF_TASKS = len(todos)
+
+done = []
+for task in todos:
+    if (task.get('completed') is True):
+        done.append(task)
+
+NUMBER_OF_DONE_TASKS = len(done)
+
+print(
+    'Employee {} is done with tasks({}/{}]):'
+    .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS)
+)
+
+[print('    {}'.format(task.get('title'))) for task in done]
