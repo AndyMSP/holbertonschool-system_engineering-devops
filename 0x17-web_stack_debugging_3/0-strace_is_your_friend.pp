fcb764538a155c60363c041fe20c3afc47b90b82
Andy <3775@holbertonschool.com>
2022-09-29 19:38:23 -0400

    fix again

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,4 @@
 # replace bad part of file
-
-exec { 'rename .phpp to .php':
-    command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
-    provider => shell
+exec { 'rename_phpp_to_php'
+    command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
 }
