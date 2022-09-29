2b71c4f95b2713f4d004a24b509de9f613010912
Andy <3775@holbertonschool.com>
2022-09-29 01:17:50 -0400

    Change technique

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# change file name
-file_line { 'remove a p':
-  ensure => present,
-  path   => '/var/www/html/wb-config.php',
-  line   => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
-  match  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );'
+# ensure file is present with proper contents
+file { 'ensure file is present':
+    ensure  => 'present',
+    path    => '/var/www/html/wp-settings.php',
+    content => file('/root/fixed_wp-settings.php')
 }
