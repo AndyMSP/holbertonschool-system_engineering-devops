220867caf15c32056d30185686e7289193d0bd25
Andy <3775@holbertonschool.com>
2022-09-28 23:58:57 -0400

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 file_line { 'bashrc_proxy':
   ensure => present,
   path   => '/var/www/html',
-  line   => 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );'
-  match  => 'require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );'
+  line   => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php' );'
+  match  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp' );'
 }
