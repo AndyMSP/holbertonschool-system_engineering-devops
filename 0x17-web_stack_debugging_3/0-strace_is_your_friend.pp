8ca9568db778b7a2dfc039ef13c0abc8887f7a51
Andy <3775@holbertonschool.com>
2022-09-29 00:09:17 -0400

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 file_line { 'remove a p':
   ensure => present,
   path   => '/var/www/html/wb-config.php',
-  line   => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php' );',
-  match  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp' );'
+  line   => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
+  match  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );'
 }
