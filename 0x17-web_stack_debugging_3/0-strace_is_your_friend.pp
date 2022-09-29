91e037cc096c908e66653c790680f730a5039ae7
Andy <3775@holbertonschool.com>
2022-09-28 23:53:46 -0400

    Add task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+#change file name
+file_line { 'bashrc_proxy':
+  ensure => present,
+  path   => '/var/www/html',
+  line   => 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );'
+  match  => 'require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );'
+}
