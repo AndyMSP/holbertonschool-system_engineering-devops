5aa9c5609cdd3c0788fcd7d23de821a08a208596
Andy <3775@holbertonschool.com>
2022-10-01 19:53:18 -0400

    Add task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""recursively finds number of hot posts in a subreddit"""
+
+
+def recurse(subreddit, hot_list=[], after='blank'):
+    """recursively finds number of hot posts in a subreddit"""
+    import requests
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'user-agent': 'andy'}
+    params = {'limit': '100', 'after': after}
+    res = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+        )
+    after = res.json()['data']['after']
+
+    # if (res.status_code != 200):
+    #     print('hello')
+    if (after is None):
+        for post in res.json()['data']['children']:
+            hot_list.append(post['data']['title'])
+        return (hot_list)
+    else:
+        for post in res.json()['data']['children']:
+            hot_list.append(post['data']['title'])
+        after = res.json()['data']['after']
+        return (recurse(subreddit, hot_list=hot_list, after=after))
