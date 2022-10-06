16df338b9c604b081fda7f817f50246840eb8a1a
Andy <3775@holbertonschool.com>
2022-10-06 17:19:13 -0400

    fix new line issue

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,6 @@
 # Replaces content of file
 file { '/etc/security/limit.conf':
     ensure  => 'present',
-    content => 'holberton hard nofile 5000\nholberton soft nofile 5000'
+    content => 'holberton hard nofile 5000
+    holberton soft nofile 5000'
 }
